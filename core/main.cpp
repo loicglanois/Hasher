25cf3e4bd990623385c0b7f10b206fc4cb6059cc
loicglanois <loic.glanois@protonmail.com>
2025-07-17 18:55:31 +0200

    Feat : Options parsing

--- core/main.cpp
+++ core/main.cpp
@@ -1,12 +1,75 @@
 #include <iostream>
 using namespace std;
 
+void parseArguments(int argc, char *argv[], string &keyword1, string &keyword2, int &magicNumber, int &truncateLength, bool &noLetters, bool &noDigits, bool &noCapitals, bool &noSymbols, string &outputFile) {
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
+        if (arg == "--keyword1" && i + 1 < argc) {
+            keyword1 = argv[++i];
+        } else if (arg == "--keyword2" && i + 1 < argc) {
+            keyword2 = argv[++i];
+        } else if (arg == "--magic" && i + 1 < argc) {
+            magicNumber = stoi(argv[++i]);
+        } else if (arg == "--truncate" && i + 1 < argc) {
+            truncateLength = stoi(argv[++i]);
+        } else if (arg == "--noletters") {
+            noLetters = true;
+        } else if (arg == "--nodigits") {
+            noDigits = true;
+        } else if (arg == "--nocapitals") {
+            noCapitals = true;
+        } else if (arg == "--nosymbols") {
+            noSymbols = true;
+        } else if (arg == "--output" && i + 1 < argc) {
+            outputFile = argv[++i];
+        }
+    }
+}
+
+void test_enumerate_modes(const string &keyword1, const string &keyword2, int magicNumber, int truncateLength, bool noLetters, bool noDigits, bool noCapitals, bool noSymbols, const string &outputFile) {
+    // Placeholder for the actual implementation of the function
+    cout << "Testing with parameters:" << endl;
+    cout << "Keyword1: " << keyword1 << endl;
+    cout << "Keyword2: " << keyword2 << endl;
+    cout << "Magic Number: " << magicNumber << endl;
+    cout << "Truncate Length: " << truncateLength << endl;
+    cout << "No Letters: " << (noLetters ? "Yes" : "No") << endl;
+    cout << "No Digits: " << (noDigits ? "Yes" : "No") << endl;
+    cout << "No Capitals: " << (noCapitals ? "Yes" : "No") << endl;
+    cout << "No Symbols: " << (noSymbols ? "Yes" : "No") << endl;
+    cout << "Output File: " << outputFile << endl;
+
+    // Here you would implement the logic to generate hashes or whatever the function is supposed to do
+}
+
 // main() function: where the execution of
 // C++ program begins
 int main(int argc, char * argv[]) {
   
-    // This statement prints "Hello World"
-    cout << "Hello World";
+    // Parse les arguments de la ligne de commande et active les modes appropriés
+    if (argc < 4) {
+        cout << "Usage: " << argv[0] << " <options>" << endl;
+        cout << "Options:" << endl;
+        cout << "  --keyword1 <keyword1>   (Mandatory) Specify the first keyword" << endl;
+        cout << "  --keyword2 <keyword2>   (Mandatory) Specify the second keyword" << endl;
+        cout << "  --magic <magic_number>  Specify a magic number" << endl;
+        cout << "  --truncate <length>     Truncate output to specified length" << endl;
+        cout << "  --noletters             Exclude letters from output" << endl;
+        cout << "  --nodigits              Exclude digits from output" << endl;
+        cout << "  --nocapitals            Exclude capital letters from output" << endl;
+        cout << "  --nosymbols             Exclude symbols from output" << endl;
+        cout << "  --output <output_file>  Specify output file" << endl;
+        return 1;
+    }
+
+    // Parsing
+    string keyword1, keyword2, outputFile;
+    static int magicNumber = 0;
+    static int truncateLength = 0;
+    static bool noLetters = false, noDigits = false, noCapitals = false, noSymbols = false;
+    parseArguments(argc, argv, keyword1, keyword2, magicNumber, truncateLength, noLetters, noDigits, noCapitals, noSymbols, outputFile);
+
+    test_enumerate_modes(keyword1, keyword2, magicNumber, truncateLength, noLetters, noDigits, noCapitals, noSymbols, outputFile);
 
     return 0;
 }
